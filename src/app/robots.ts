7cc3e2ebc85e0076bd7089e0129685ee236c3606
ghadirli <aghadirl@uwaterloo.ca>
2025-04-30 22:39:37 -0400

    updated robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,15 @@
 // app/robots.ts
-import {MetadataRoute} from 'next'
+import { MetadataRoute } from 'next'
+
+// Add this line to make it compatible with static export
+export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
-        sitemap: 'https://ghadirli.github.io/sitemap.xml',
-    }
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://ghadirli.github.io/sitemap.xml',
+  }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,16 @@
 // app/sitemap.ts
-import {MetadataRoute} from 'next'
+import { MetadataRoute } from 'next'
+
+// Add this line to make it compatible with static export
+export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: 'https://ghadirli.github.io',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 1.0,
-        },
-    ]
+  return [
+    {
+      url: 'https://ghadirli.github.io',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+  ]
 }
